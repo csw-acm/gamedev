fe90f0d08c0376a0bd55b86286b534119e6ee916
BuildTools <jaydonreap@gmail.com>
2018-11-15 17:03:11 -0500

    Added Lesson 4 Assets

---
+++ Lesson 4 Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+/*
+ * ACM UI demo
+ * Game Dev Lesson #4
+ * STUDENT VERSION
+ * Copyright (c) 2018 Jaydon Reap (Ficus Technologies)
+*/
+
+// This is the main class, we covered what most of this was last lesson
+public class MenuController : MonoBehaviour
+{
+    // New variable types, woo!
+    
+
+
+
+    // Here we get all our button listeners added
+    // Listeners are essentially what the name implies, they wait and "listen" for a certain input
+    // in the case of our button thats a mouse click
+    void Start()
+    {
+        
+    }
+}
